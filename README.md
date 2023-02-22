b6f59a1c811c52c07a21e723a824f4baec2e803e
Artem Sakhatskiy <artem@finmid.com>
2023-02-22 20:08:04 +0100

    docs: improve README docs

--- README.md
+++ README.md
@@ -13,3 +13,18 @@ pnpm i
 ## Command-line interface
 
 Run training and control via `pnpm start`
+
+```sh
+pnpm start [set] [target] [epochs]
+
+fit model from fit.csv set and predict against control.csv
+
+Positionals:
+  set     name of the set folder in ./data/<set> folder    [string] [default: "example"]
+  target  target column name                               [string] [default: "medv"]
+  epochs  amount of epochs for fitting                     [number] [default: 50]
+
+Options:
+  --version  Show version number                           [boolean]
+  --help     Show help                                     [boolean]
+```
--- src/start.ts
+++ src/start.ts
@@ -3,36 +3,44 @@ import "./initialize";
 
 console.log("\n\n");
 
-Yargs.scriptName("workshop")
-  .usage("$0 <cmd> [args]")
-  .command(
-    "csv [set] [target] [epochs]",
-    "fit model from fit.csv set and predict against control.csv",
-    (yargs) => {
-      yargs.positional("set", {
-        type: "string",
-        default: "example",
-        describe: "name of the set folder in ./data/<set> folder",
-      });
-      yargs.positional("target", {
-        type: "string",
-        default: "medv",
-        describe: "target column name",
-      });
-      yargs.positional("epochs", {
-        type: "number",
-        default: 50,
-        describe: "amount of epochs for fitting",
-      });
-    },
-    async function (argv) {
-      const { csv } = await import("./csv");
+Yargs.command(
+  "$0 [set] [target] [epochs]",
+  "fit model from fit.csv set and predict against control.csv",
+  (yargs) => {
+    yargs.positional("set", {
+      type: "string",
+      demandOption: true,
+      default: "example",
+      describe: "name of the set folder in ./data/<set> folder",
+    });
+    yargs.positional("target", {
+      type: "string",
+      default: "medv",
+      describe: "target column name",
+    });
+    yargs.positional("epochs", {
+      type: "number",
+      default: 50,
+      describe: "amount of epochs for fitting",
+    });
+  },
+  async function (argv) {
+    const { csv } = await import("./csv");
 
-      await csv({
-        set: argv["set"] as string,
-        target: argv["target"] as string,
-        epochs: argv["epochs"] as number | undefined,
-      });
-    }
-  )
-  .help().argv;
+    const set = argv["set"] as string;
+    const target = argv["target"] as string;
+    const epochs = argv["epochs"] as number | undefined;
+
+    console.info(
+      `Running fitting against "${set}" set, "${target}" target, with ${epochs} epochs.`
+    );
+
+    console.log("\n\n");
+
+    await csv({
+      set,
+      target,
+      epochs,
+    });
+  }
+).help().argv;
